725b7069621b61f9d3c08cd8ba0f75218ccc8665
Paawan khandelwal <81919527+paawankhandelwal@users.noreply.github.com>
2021-08-19 09:28:07 -0700

    Add files via upload

---
+++ id17282656_task.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 21, 2021 at 02:16 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id17282656_task`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `account_no` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `balance` decimal(6,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `account_no`, `email`, `mobile`, `balance`) VALUES
+(1, 'Paawan Khandelwal', 'XXXXXXXXXXXXX07270', 'paawan.khandelwal@gmail.com', '8209984120', 3038),
+(2, 'Badal Chhipa', 'XXXXXXXXXXXXX3919', 'badalchhipa@gmail.com', '8386868450', 3919),
+(3, 'Diviii Jain', 'XXXXXXXXXXXXX4545', 'diviiijain23@gmail.com', '8584627015', 4545),
+(4, 'Bittu Gupta', 'XXXXXXXXXXXXX6969', 'bittugupta69@gmail.com', '8093080930', 6969),
+(5, 'Golu Yadav', 'XXXXXXXXXXXXX2525', 'goluyadav25@gmail.com', '7892456130', 2525);
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `from` varchar(255) NOT NULL,
+  `to` varchar(255) NOT NULL,
+  `amount` decimal(65,0) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
